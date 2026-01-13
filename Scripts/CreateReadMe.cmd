9af6aff94344bfdd83fb46c2a2bec9caba377a01
Daniel Patterson <danielanywhere@users.noreply.github.com>
2026-01-13 09:05:04 -0600

    Update FindAndReplace Link

--- Scripts/CreateReadMe.cmd
+++ Scripts/CreateReadMe.cmd
@@ -1,7 +1,7 @@
 :: CreateReadMe.cmd
 :: Create the ReadMe.md file from Docs/ReadMe.docx.
 :: This command is meant to be run from within the Scripts folder.
-SET FAR=C:\Files\Dropbox\Develop\Active\FindAndReplace\FindAndReplace\bin\Debug\net6.0\FindAndReplace.exe
+SET FAR=C:\Files\Dropbox\Develop\Shared\FindAndReplace\Source\FindAndReplace\bin\Debug\net6.0\FindAndReplace.exe
 SET SOURCE=..\Docs\ReadMe.odt
 SET TARGET=..\README.md
 SET PATTERN=ReadmePostProcessing.json
